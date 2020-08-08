e4f5a96c98344d2384f6aed8a0cb8baed8f99fbb
Fenichel <eli.fenichel@yale.edu>
2020-08-08 15:20:10 -0400

    cleaned up and more figures.

--- pooling/analysis_figures.R
+++ pooling/analysis_figures.R
@@ -65,19 +65,42 @@ p.20.10k.cil <- prev.results(20, 10000, other.data = pool.sen.cil)
 # p.10.100k <- prev.results(10, 100000)
 # p.20.100k <- prev.results(20, 100000)
 
+
+p.10.10.k.cis <-as.data.frame(cbind(p.10.10k.cil$prevalence, p.10.10k.cil$total.tests, p.10.10k.ciu$total.tests))
+colnames(p.10.10.k.cis) <- c("prevalence", "cil", "ciu")
+p.20.10.k.cis <-as.data.frame(cbind(p.20.10k.cil$prevalence, p.20.10k.cil$total.tests, p.20.10k.ciu$total.tests))
+colnames(p.20.10.k.cis) <- c("prevalence", "cil", "ciu")
+p.5.10.k.cis <-as.data.frame(cbind(p.5.10k.cil$prevalence, p.5.10k.cil$total.tests, p.5.10k.ciu$total.tests))
+colnames(p.5.10.k.cis) <- c("prevalence", "cil", "ciu")
+
 plot10k <- ggplot() +
-  geom_line(data = p.10.10k, aes(x = prevalence, y = total.tests ), color = "#85c4b9", size = 1.25) +
-  geom_line(data = p.20.10k, aes(x = prevalence, y = total.tests), color = "#afd88d", size = 1.25) +
-  geom_line(data = p.5.10k, aes(x = prevalence, y = total.tests ), color = '#27496a', size = 1.25) +
-  geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = total.tests ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.10.10k.cil, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.cil, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.cil, aes(x = prevalence, y = total.tests ), color = "#27496a", linetype = 'dashed', size = 0.75) +
+  geom_line(data = p.10.10k, aes(x = prevalence, y = total.tests ), 
+            color = "#85c4b9", size = 1.25) +
+  geom_line(data = p.20.10k, aes(x = prevalence, y = total.tests), 
+            color = "#afd88d", size = 1.25) +
+  geom_line(data = p.5.10k, aes(x = prevalence, y = total.tests ), 
+            color = '#27496a', size = 1.25) +
+  # geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = total.tests ), 
+  #           color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = total.tests), 
+  #           color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = total.tests ), 
+  #           color = '#27496a', linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.10.10k.cil, aes(x = prevalence, y = total.tests ), 
+  #           color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.cil, aes(x = prevalence, y = total.tests), 
+  #           color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.cil, aes(x = prevalence, y = total.tests ), 
+  #           color = "#27496a", linetype = 'dashed', size = 0.75) +
+  geom_ribbon(data=p.5.10.k.cis,
+            aes(x = prevalence, ymin=cil,ymax=ciu), fill='#27496a', alpha = 0.4) +
+  geom_ribbon(data=p.10.10.k.cis,
+              aes(x = prevalence, ymin=cil,ymax=ciu), fill="#85c4b9", alpha = 0.4) +
+  geom_ribbon(data=p.20.10.k.cis,
+              aes(x = prevalence, ymin=cil,ymax=ciu), fill="#afd88d", alpha = 0.4) +
   annotate(geom="text",x=0.1,y= 7520 ,label="pool = 10",color="#85c4b9",  angle = 40) +
   annotate(geom="text",x=0.073,y= 8400 ,label="pool = 20",color="#afd88d", angle = 44) +
-  annotate(geom="text",x=0.16,y= 8000 ,label="pool = 5",color="#27496a", angle = 25) +
+  annotate(geom="text",x=0.16,y= 8000 ,label="pool = 5",color="#27496a", angle = 30) +
   annotate(geom="text",x=0.04,y= 10300,label="pool = 1",color='#000000') +
   labs( 
     x= "Prevalence",
@@ -92,12 +115,18 @@ plot10k.zoom <- ggplot() +
   geom_line(data = p.10.10k, aes(x = prevalence, y = total.tests ), color = "#85c4b9", size = 1.25) +
   geom_line(data = p.20.10k, aes(x = prevalence, y = total.tests), color = "#afd88d", size = 1.25) +
   geom_line(data = p.5.10k, aes(x = prevalence, y = total.tests ), color = '#27496a', size = 1.25) +
-  geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = total.tests ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.10.10k.cil, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.cil, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.cil, aes(x = prevalence, y = total.tests ), color = "#27496a", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = total.tests ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.10.10k.cil, aes(x = prevalence, y = total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.cil, aes(x = prevalence, y = total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.cil, aes(x = prevalence, y = total.tests ), color = "#27496a", linetype = 'dashed', size = 0.75) +
+  geom_ribbon(data=p.5.10.k.cis,
+              aes(x = prevalence, ymin=cil,ymax=ciu), fill='#27496a', alpha = 0.4) +
+  geom_ribbon(data=p.10.10.k.cis,
+              aes(x = prevalence, ymin=cil,ymax=ciu), fill="#85c4b9", alpha = 0.4) +
+  geom_ribbon(data=p.20.10.k.cis,
+              aes(x = prevalence, ymin=cil,ymax=ciu), fill="#afd88d", alpha = 0.4) +
   labs(
     x= "",
     y = "")  +
@@ -142,9 +171,6 @@ plot10k.undetected <- ggplot() +
 plot10k.undetected
 
 
-
-
-
 plot10k.undetected.zoom <- ggplot() +
   geom_line(data = p.10.10k, aes(x = prevalence, y = numb.undetected.pos ), color = "#85c4b9", size = 1.25) +
   geom_line(data = p.20.10k, aes(x = prevalence, y = numb.undetected.pos), color = "#afd88d", size = 1.25) +
@@ -174,7 +200,7 @@ plot_grid(plot10k.undetected,
           labels = "AUTO", ncol = 2 )
 
 #======================================================================================
-
+#The cost savings 
 10000 - p.10.10k$total.tests
 
 costplot <- ggplot() +
@@ -219,7 +245,12 @@ costplot +
 #==============================================================================
 #Detection per test
 
-names(p.10.10k)
+p.10.10.k.cis <-as.data.frame(cbind(p.10.10k.cil$prevalence, p.10.10k.cil$pos.found, p.10.10k.ciu$pos.found, p.10.10k.cil$total.tests, p.10.10k.ciu$total.tests))
+colnames(p.10.10.k.cis) <- c("prevalence", "cil.pf", "ciu.pf", "cil.tt", "ciu.tt")
+p.20.10.k.cis <-as.data.frame(cbind(p.20.10k.cil$prevalence, p.20.10k.cil$pos.found, p.20.10k.ciu$pos.found, p.20.10k.cil$total.tests, p.20.10k.ciu$total.tests))
+colnames(p.20.10.k.cis) <- c("prevalence", "cil.pf", "ciu.pf", "cil.tt", "ciu.tt")
+p.5.10.k.cis <-as.data.frame(cbind(p.5.10k.cil$prevalence, p.5.10k.cil$pos.found, p.5.10k.ciu$pos.found, p.5.10k.cil$total.tests, p.5.10k.ciu$total.tests))
+colnames(p.5.10.k.cis) <- c("prevalence", "cil.pf", "ciu.pf", "cil.tt", "ciu.tt")
 
 p.10.10k$pos.found/ p.10.10k$total.tests
 
@@ -227,13 +258,19 @@ plot10k.pos.test <- ggplot() +
   geom_line(data = p.10.10k, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", size = 1.25) +
   geom_line(data = p.20.10k, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", size = 1.25) +
   geom_line(data = p.5.10k, aes(x = prevalence, y = pos.found/total.tests  ), color = '#27496a', size = 1.25) +
-  geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = pos.found/total.tests  ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.10.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.cil, aes(x = prevalence, y = pos.found/total.tests), color = "#27496a", linetype = 'dashed', size = 0.75) +
-  geom_abline(intercept = 0, slope =1, color = "black") +
+  # geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = pos.found/total.tests  ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.10.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.cil, aes(x = prevalence, y = pos.found/total.tests), color = "#27496a", linetype = 'dashed', size = 0.75) +
+  geom_ribbon(data=p.5.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill='#27496a', alpha = 0.4) +
+  geom_ribbon(data=p.10.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill="#85c4b9", alpha = 0.4) +
+  geom_ribbon(data=p.20.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill="#afd88d", alpha = 0.4) +
+  geom_abline(intercept = 0, slope =1, color = "black", size = 1.25) +
   annotate(geom="text",x=0.1,y= 0.15 ,label="pool = 10",color="#85c4b9",  angle = 30) +
   annotate(geom="text",x=0.07,y= 0.105 ,label="pool = 20",color="#afd88d", angle = 30) +
   annotate(geom="text",x=0.15,y= 0.21, label="pool = 5",color="#27496a", angle = 30) +
@@ -257,13 +294,20 @@ plot10k.pos.test.zoom <- ggplot() +
   geom_line(data = p.10.10k, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", size = 1.25) +
   geom_line(data = p.20.10k, aes(x = prevalence, y = pos.found/total.tests ), color = "#afd88d", size = 1.25) +
   geom_line(data = p.5.10k, aes(x = prevalence, y = pos.found/total.tests  ), color = '#27496a', size = 1.25) +
-  geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = pos.found/total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.10.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
-  geom_line(data = p.20.10k.cil, aes(x = prevalence, y = pos.found/total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
-  geom_line(data = p.5.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#27496a", linetype = 'dashed', size = 0.75) +
-  geom_abline(intercept = 0, slope =1, color = "black") +
+  # geom_line(data = p.10.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.ciu, aes(x = prevalence, y = pos.found/total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.ciu, aes(x = prevalence, y = pos.found/total.tests ), color = '#27496a', linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.10.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#85c4b9", linetype = 'dashed', size = 0.75) +
+  # geom_line(data = p.20.10k.cil, aes(x = prevalence, y = pos.found/total.tests), color = "#afd88d", linetype = 'dashed', size = 0.75) + 
+  # geom_line(data = p.5.10k.cil, aes(x = prevalence, y = pos.found/total.tests ), color = "#27496a", linetype = 'dashed', size = 0.75) +
+  geom_ribbon(data=p.5.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill='#27496a', alpha = 0.4) +
+  geom_ribbon(data=p.10.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill="#85c4b9", alpha = 0.4) +
+  geom_ribbon(data=p.20.10.k.cis,
+              aes(x = prevalence, ymin=cil.pf/cil.tt,ymax=ciu.pf/ciu.tt), fill="#afd88d", alpha = 0.4) +
+  geom_abline(intercept = 0, slope =1, color = "black", size = 1.25) +
+  
   # annotate(geom="text",x=0.15,y= 52 ,label="pool = 10",color="#85c4b9",  angle = 0) +
   # annotate(geom="text",x=0.2,y= 15 ,label="pool = 20",color="#afd88d", angle = 0) +
   # annotate(geom="text",x=0.22,y= 68 ,label="pool = 5",color="#27496a", angle = 0) +
@@ -284,29 +328,57 @@ plot10k.pos.test.zoom
 
 plot10k.pos.test +
   annotation_custom(ggplotGrob(plot10k.pos.test.zoom), xmin = 0.12, xmax = 0.32, 
-                    ymin = -0.03, ymax = 0.17)
+                    ymin = -0.02, ymax = 0.15)
 
-#======================================================================================
 
-10000 - p.10.10k$total.tests
 
-costplot <- ggplot() +
-  geom_line(data = p.10.10k, aes(x = prevalence, y = 3*(10000 - total.tests)), color = "#85c4b9", size = 1.25) +
-  geom_line(data = p.20.10k, aes(x = prevalence, y = 3*(10000 - total.tests)), color = "#afd88d", size = 1.25) + 
-  geom_line(data = p.5.10k, aes(x = prevalence, y = 3*(10000 - total.tests)), color = '#27496a', size = 1.25) + 
-  annotate(geom="text",x=0.1,y= 13800 ,label="pool = 5",color='#27496a', angle = -38) +
-  annotate(geom="text",x=0.15,y= 5500 ,label="pool = 10",color="#85c4b9", angle = -35) +
-  annotate(geom="text",x=0.18,y= 740 ,label="pool = 20",color="#afd88d", angle = -10) +
+
+#####
+#cost savings
+
+
+plot10k.pos.test.c <- ggplot() +
+  geom_line(data = p.10.10k, aes(x = prevalence, y = 30*(-total.tests / pos.found + 1/prevalence ) ), color = "#85c4b9", size = 1.25) +
+  geom_line(data = p.20.10k, aes(x = prevalence, y = 30*(-total.tests /pos.found  + 1/prevalence  )), color = "#afd88d", size = 1.25) +
+  geom_line(data = p.5.10k, aes(x = prevalence, y = 30*(-total.tests /pos.found   + 1/prevalence  )), color = '#27496a', size = 1.25) +
   labs( 
     x= "Prevalence",
-    y = "Cost Savings, assumng $3 per test")  +
+    y = "The reduction in the average cost \n per positive individual relative to \n individual tests, assuming $30 per test ")  +
+  #ggtitle("population 10,000") +
+  annotate(geom="text",x=0.125,y= 60 ,label="pool = 10",color="#85c4b9",  angle = -35) +
+  annotate(geom="text",x=0.10,y= 52 ,label="pool = 20",color="#afd88d", angle = -45) +
+  annotate(geom="text",x=0.25,y= 22 ,label="pool = 5",color="#27496a", angle = -10) +
   theme_classic(base_size = 15) +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  scale_y_continuous(labels = dollar)
+  #geom_hline(yintercept = 10000, linetype = "solid", color = "red", size = 0.5) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10), limits = c(0, 0.30)) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10), limits = c(0, 500), labels = dollar) 
+
+plot10k.pos.test.c 
+
+
+plot10k.pos.test.c.zoom <- ggplot() +
+  geom_line(data = p.10.10k, aes(x = prevalence, y = 30*(-total.tests / pos.found + 1/prevalence ) ), color = "#85c4b9", size = 1.25) +
+  geom_line(data = p.20.10k, aes(x = prevalence, y = 30*(-total.tests /pos.found  + 1/prevalence  )), color = "#afd88d", size = 1.25) +
+  geom_line(data = p.5.10k, aes(x = prevalence, y = 30*(-total.tests /pos.found   + 1/prevalence  )), color = '#27496a', size = 1.25) +
+  labs( 
+    x= "",
+    y = "")  +
+  #ggtitle("population 10,000") +
+  theme(
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA),
+    axis.line = element_line(colour = "black")
+  )+
+  #geom_hline(yintercept = 10000, linetype = "solid", color = "red", size = 0.5) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5), limits = c(0, 0.05)) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10), limits = c(0, 20000), labels = dollar) 
+plot10k.pos.test.c.zoom
 
 
 
-
+plot10k.pos.test.c +
+  annotation_custom(ggplotGrob(
+    plot10k.pos.test.c.zoom), xmin = 0.10, xmax = 0.3, ymin = 50, ymax = 520)
 
 #==============================================================================
 #Tests constrained
